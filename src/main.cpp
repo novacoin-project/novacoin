a7a898d738c37fbc59257aa11216fee6e7cdbfec
Philip Kaufmann <phil.kaufmann@t-online.de>
2012-10-21 21:16:52 +0200

    remove init messages from ThreadImport()
    - remove uiInterface.InitMessage() calls from ThreadImport(), as Qt
      doesn't like them getting called out of it's main thread and because the
      thread will continue to run after the GUI was loaded
    
    Conflicts:
    	src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2727,7 +2727,6 @@ void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
     CImportingNow imp;
 
     // -loadblock=
-    uiInterface.InitMessage(_("Starting block import..."));
     BOOST_FOREACH(boost::filesystem::path &path, vImportFiles) {
         FILE *file = fopen(path.string().c_str(), "rb");
         if (file)
@@ -2737,8 +2736,6 @@ void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
     // hardcoded $DATADIR/bootstrap.dat
     filesystem::path pathBootstrap = GetDataDir() / "bootstrap.dat";
     if (filesystem::exists(pathBootstrap)) {
-        uiInterface.InitMessage(_("Importing bootstrap blockchain data file."));
-
         FILE *file = fopen(pathBootstrap.string().c_str(), "rb");
         if (file) {
             filesystem::path pathBootstrapOld = GetDataDir() / "bootstrap.dat.old";
