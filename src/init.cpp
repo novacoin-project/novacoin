fe10adce4f3c7e2229e4926f2866ff02f71a2d83
Eric Lombrozo <elombrozo@gmail.com>
2013-01-07 08:07:51 -0800

    Removed net.cpp's dependency on init.h.
    Added explicit include of main.h in init.cpp, changed include of init.h to include of main.h in net.cpp.
    
    Added function registration for net.cpp in init.cpp's network initialization.
    
    Removed protocol.cpp's dependency on main.h.
    
    TODO: Remove main.h include in net.cpp.
    
    Conflicts:
    	src/init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -2,11 +2,13 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "init.h"
+#include "main.h"
 #include "txdb.h"
 #include "walletdb.h"
 #include "bitcoinrpc.h"
 #include "net.h"
-#include "init.h"
 #include "util.h"
 #include "ui_interface.h"
 #include "checkpoints.h"
@@ -459,6 +461,10 @@ bool AppInit2(boost::thread_group& threadGroup)
     }
 
     // ********************************************************* Step 6: network initialization
+
+    SetProcessMessagesHandler(ProcessMessages);
+    SetSendMessagesHandler(SendMessages);
+    SetStartShutdownHandler(StartShutdown);
 
     int nSocksVersion = GetArg("-socks", 5);
 
