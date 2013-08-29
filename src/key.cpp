0cce7e7e8d5da9344988cdd3f8291f7708160f95
patrick s <patrick.strateman@gmail.com>
2013-08-29 01:11:47 -0700

    verify vchPubKey matches calculated public key unless fSkipCheck is set
    Conflicts:
    	src/key.cpp

--- src/key.cpp
+++ src/key.cpp
@@ -418,6 +418,12 @@ bool CKey::Load(CPrivKey &privkey, CPubKey &vchPubKey, bool fSkipCheck=false) {
     fCompressed = vchPubKey.IsCompressed();
     fValid = true;
 
+    if (fSkipCheck)
+        return true;
+
+    if (GetPubKey() != vchPubKey)
+        return false;
+
     return true;
 }
 
