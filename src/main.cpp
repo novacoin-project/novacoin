18a7a330ee5de2dd06ccfc4a824bdbe55ed36eaa
alex <alex@alex-VirtualBox.(none)>
2014-01-23 00:03:35 +0400

    Fix segfault on testnet

--- src/main.cpp
+++ src/main.cpp
@@ -1821,7 +1821,7 @@ bool SetBestChain(CBlockIndex* pindexNew)
 
     // special case for attaching the genesis block
     // note that no ConnectBlock is called, so its coinbase output is non-spendable
-    if (pindexGenesisBlock == NULL && pindexNew->GetBlockHash() == hashGenesisBlock)
+    if (pindexGenesisBlock == NULL && pindexNew->GetBlockHash() == (!fTestNet ? hashGenesisBlock : hashGenesisBlockTestNet))
     {
         view.SetBestBlock(pindexNew);
         if (!view.Flush())
