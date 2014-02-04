ae7d505f0045672c520a1548c6d5ca2b728d5e79
MASM fan <masmfan@gmail.com>
2014-02-04 21:29:58 +0400

    BugFix: free transactions are not allowed yet

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1413,8 +1413,7 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, int64> >& vecSend, CW
 
                 // Check that enough fee is included
                 int64 nPayFee = nTransactionFee * (1 + (int64)nBytes / 1000);
-                bool fAllowFree = CTransaction::AllowFree(dPriority);
-                int64 nMinFee = wtxNew.GetMinFee(1, fAllowFree, GMF_SEND);
+                int64 nMinFee = wtxNew.GetMinFee(1, false, GMF_SEND, nBytes);
                 if (nFeeRet < max(nPayFee, nMinFee))
                 {
                     nFeeRet = max(nPayFee, nMinFee);
