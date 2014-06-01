a2bdc7a6c33409df800b8ffcc14b5e8e2d5eb0a8
CryptoManiac <balthazar@yandex.ru>
2014-06-01 16:39:21 +0400

    Force allowfree = false until 1 july 2014

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1534,6 +1534,12 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, int64> >& vecSend, CW
                 // Check that enough fee is included
                 int64 nPayFee = nTransactionFee * (1 + (int64)nBytes / 1000);
                 bool fAllowFree = CTransaction::AllowFree(dPriority);
+
+                if (!fTestNet && wtxNew.nTime < FEE_SWITCH_TIME)
+                {
+                    fAllowFree = false;
+                }
+
                 int64 nMinFee = wtxNew.GetMinFee(1, fAllowFree, GMF_SEND, nBytes);
                 if (nFeeRet < max(nPayFee, nMinFee))
                 {
