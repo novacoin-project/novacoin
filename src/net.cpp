956a1283d9e70bdeab75ea44d69f990fc2d5f988
blkmaarx <mpvisser91@gmail.com>
2014-07-21 09:48:52 +0200

    remove maarx.blackcoin.co seed
    it is offline for time being

--- src/net.cpp
+++ src/net.cpp
@@ -1252,7 +1252,6 @@ void MapPort()
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
     {"rat4.blackcoin.co", "seed.blackcoin.co"},
-    {"maarx.blackcoin.co", "seed2.blackcoin.co"},
     {"archon.darkfox.id.au", "foxy.seeds.darkfox.id.au"},
 };
 
