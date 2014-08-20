e63953a4de980b1783cf9de9a2e50fb1cd959941
Wladimir J. van der Laan <laanwj@gmail.com>
2014-08-20 10:51:18 +0200

    Move strprintf define to tinyformat.h
    This avoids a dependency on util.h if just tinyformat is needed.
    
    Conflicts:
    	src/core.cpp

--- src/tinyformat.h
+++ src/tinyformat.h
@@ -1008,4 +1008,6 @@ TINYFORMAT_WRAP_FORMAT_N(16, returnType, funcName, funcDeclSuffix, bodyPrefix, s
 
 } // namespace tinyformat
 
+#define strprintf tfm::format
+
 #endif // TINYFORMAT_H_INCLUDED
