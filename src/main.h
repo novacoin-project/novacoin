9e6e289443628abefbd789a719df39c95311e6bc
Pavel Vasin <pavel@vasin.nl>
2014-12-11 20:37:17 +0300

    add switch for protocol v3

--- src/main.h
+++ src/main.h
@@ -55,6 +55,7 @@ static const int64_t COIN_YEAR_REWARD = 1 * CENT; // 1% per year
 
 inline bool IsProtocolV1RetargetingFixed(int nHeight) { return TestNet() || nHeight > 38423; }
 inline bool IsProtocolV2(int nHeight) { return TestNet() || nHeight > 319000; }
+inline bool IsProtocolV3(int64_t nTime) { return TestNet() || nTime > 1444028400; }
 
 inline int64_t FutureDriftV1(int64_t nTime) { return nTime + 10 * 60; }
 inline int64_t FutureDriftV2(int64_t nTime) { return nTime + 15; }
