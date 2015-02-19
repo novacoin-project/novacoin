3812311d8ef489d3ffe062f0d7080a2d702a0e9c
Pavel Vasin <pavel@vasin.nl>
2015-02-19 17:26:36 +0300

    fix off-by-one in GetPoSKernelPS()

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -85,10 +85,13 @@ double GetPoSKernelPS()
     {
         if (pindex->IsProofOfStake())
         {
-            dStakeKernelsTriedAvg += GetDifficulty(pindex) * 4294967296.0;
-            nStakesTime += pindexPrevStake ? (pindexPrevStake->nTime - pindex->nTime) : 0;
+            if (pindexPrevStake)
+            {
+                dStakeKernelsTriedAvg += GetDifficulty(pindexPrevStake) * 4294967296.0;
+                nStakesTime += pindexPrevStake->nTime - pindex->nTime;
+                nStakesHandled++;
+            }
             pindexPrevStake = pindex;
-            nStakesHandled++;
         }
 
         pindex = pindex->pprev;
