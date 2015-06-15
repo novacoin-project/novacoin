dc64a6b3286763218281670b6573c3d2089d9ac0
Pavel Vasin <pavel@vasin.nl>
2015-06-15 15:35:52 +0300

    Revert "locks exhaustion fix"
    This reverts commit 16e1262471dba899f3e9251b485abec219aec844.

--- src/db.cpp
+++ src/db.cpp
@@ -84,11 +84,7 @@ bool CDBEnv::Open(boost::filesystem::path pathEnv_)
     dbenv.set_cachesize(nDbCache / 1024, (nDbCache % 1024)*1048576, 1);
     dbenv.set_lg_bsize(1048576);
     dbenv.set_lg_max(10485760);
-
-    // Bugfix: Bump lk_max_locks default to 537000, to safely handle reorgs with up to 5 blocks reversed
-    // dbenv.set_lk_max_locks(10000);
-    dbenv.set_lk_max_locks(537000);
-
+    dbenv.set_lk_max_locks(10000);
     dbenv.set_lk_max_objects(10000);
     dbenv.set_errfile(fopen(pathErrorFile.string().c_str(), "a")); /// debug
     dbenv.set_flags(DB_AUTO_COMMIT, 1);
