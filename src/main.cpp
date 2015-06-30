0774da054e29ea901083984bd8f1ed0e29aeba60
Pavel Vasin <pavel@vasin.nl>
2015-06-30 17:20:06 +0300

    demand canonical block sig from updated peers

--- src/main.cpp
+++ src/main.cpp
@@ -2193,10 +2193,13 @@ bool ProcessBlock(CNode* pfrom, CBlock* pblock)
     }
 
     // Block signature can be malleated in such a way that it increases block size up to maximum allowed by protocol
-    // For now we just strip garbage from newly received blocks
     if (!IsCanonicalBlockSignature(pblock)) {
-        if (!ReserealizeBlockSignature(pblock))
+        if (pfrom && pfrom->nVersion >= CANONICAL_BLOCK_SIG_VERSION) {
+            pfrom->Misbehaving(100);
+            return error("ProcessBlock(): bad block signature encoding");
+        } else if (!ReserealizeBlockSignature(pblock)) {
             LogPrintf("WARNING: ProcessBlock() : ReserealizeBlockSignature FAILED\n");
+        }
     }
 
     // Preliminary checks
--- src/version.h
+++ src/version.h
@@ -52,4 +52,7 @@ static const int BIP0031_VERSION = 60000;
 // "mempool" command, enhanced "getdata" behavior starts with this version:
 static const int MEMPOOL_GD_VERSION = 60002;
 
+// reject blocks with non-canonical signatures starting from this version
+static const int CANONICAL_BLOCK_SIG_VERSION = 60016;
+
 #endif
