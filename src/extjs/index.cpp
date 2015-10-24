59501a1c27b7d01fdb1c26188edc04eb2d92d2f0
CryptoManiac <balthazar@yandex.ru>
2015-10-24 13:57:55 -0700

    Add Ext.ux files.

---
+++ src/extjs/index.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string extfs_items[] = 
+{
+    // Main modules
+    "/ext-all-debug.js",
+    "/ux-debug.js",
+
+    // Stylesheet
+    "/ux/ux-all-debug.css",
+    "/classic/theme-triton/resources/theme-triton-all-debug_1.css",
+    "/classic/theme-triton/resources/theme-triton-all-debug_2.css",
+
+    // Locales
+    "/classic/locale/locale-en-debug.js",
+    "/classic/locale/locale-ru-debug.js",
+    "/classic/locale/locale-ukr-debug.js",
+
+    // Fonts
+    "/fonts/OpenSans-BoldItalic.ttf",
+    "/fonts/OpenSans-Bold.ttf",
+    "/fonts/OpenSans-ExtraBoldItalic.ttf",
+    "/fonts/OpenSans-ExtraBold.ttf",
+    "/fonts/OpenSans-Italic.ttf",
+    "/fonts/OpenSans-LightItalic.ttf",
+    "/fonts/OpenSans-Light.ttf",
+    "/fonts/OpenSans-Regular.ttf",
+    "/fonts/OpenSans-SemiboldItalic.ttf",
+    "/fonts/OpenSans-Semibold.ttf",
+    "/font-awesome/fonts/FontAwesome.otf",
+    "/font-awesome/fonts/fontawesome-webfont.eot",
+    "/font-awesome/fonts/fontawesome-webfont.svg",
+    "/font-awesome/fonts/fontawesome-webfont.woff",
+    "/font-awesome/fonts/fontawesome-webfont.woff2",
+    "/font-awesome/fonts/fontawesome-webfont.ttf",
+    "/font-ext/fonts/ExtJS.ttf",
+    "/font-ext/fonts/ExtJS.woff",
+    "/font-ext/fonts/ExtJS.svg",
+    "/font-ext/fonts/ExtJS.eot",
+};
+
+bool get_file(const string& path, vector<unsigned char>& data, string& mimeType)
+{
+    
+
+    return false;
+}
