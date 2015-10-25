deea35093d93e47b0cc96bf10c927d4319a51cae
CryptoManiac <balthazar@yandex.ru>
2015-10-24 18:35:00 -0700

    Implement get_file function.

--- src/extjs/index.cpp
+++ src/extjs/index.cpp
@@ -1,50 +1,121 @@
 #include <string>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-string extfs_items[] = 
+extern "C" unsigned char __ux_debug_js[];
+extern "C" unsigned char __ext_all_debug_js[];
+extern "C" unsigned char __svg_font_awesome__fonts__fontawesome_webfont_svg[];
+extern "C" unsigned char __svg_font_ext__fonts__ExtJS_svg[];
+extern "C" unsigned char __otf_font_awesome__fonts__FontAwesome_otf[];
+extern "C" unsigned char __eot_font_ext__fonts__ExtJS_eot[];
+extern "C" unsigned char __eot_font_awesome__fonts__fontawesome_webfont_eot[];
+extern "C" unsigned char __css_classic__theme_triton__resources__theme_triton_all_debug_1_css[];
+extern "C" unsigned char __css_classic__theme_triton__resources__theme_triton_all_debug_2_css[];
+extern "C" unsigned char __css_ux__ux_all_debug_css[];
+extern "C" unsigned char __woff_font_awesome__fonts__fontawesome_webfont_woff[];
+extern "C" unsigned char __woff_font_ext__fonts__ExtJS_woff[];
+extern "C" unsigned char __woff2_font_awesome__fonts__fontawesome_webfont_woff2[];
+extern "C" unsigned char __ttf_fonts__OpenSans_BoldItalic_ttf[];
+extern "C" unsigned char __ttf_fonts__OpenSans_SemiboldItalic_ttf[];
+extern "C" unsigned char __ttf_fonts__OpenSans_Italic_ttf[];
+extern "C" unsigned char __ttf_fonts__OpenSans_ExtraBoldItalic_ttf[];
+extern "C" unsigned char __ttf_font_ext__fonts__ExtJS_ttf[];
+extern "C" unsigned char __ttf_fonts__OpenSans_ExtraBold_ttf[];
+extern "C" unsigned char __ttf_fonts__OpenSans_Regular_ttf[];
+extern "C" unsigned char __ttf_fonts__OpenSans_Semibold_ttf[];
+extern "C" unsigned char __ttf_fonts__OpenSans_Bold_ttf[];
+extern "C" unsigned char __ttf_fonts__OpenSans_LightItalic_ttf[];
+extern "C" unsigned char __ttf_font_awesome__fonts__fontawesome_webfont_ttf[];
+extern "C" unsigned char __ttf_fonts__OpenSans_Light_ttf[];
+extern "C" unsigned char __js_classic__locale__locale_ukr_debug_js[];
+extern "C" unsigned char __js_classic__locale__locale_ru_debug_js[];
+extern "C" unsigned char __js_classic__locale__locale_en_debug_js[];
+
+extern "C" unsigned int __ux_debug_js_len;
+extern "C" unsigned int __ext_all_debug_js_len;
+extern "C" unsigned int __svg_font_awesome__fonts__fontawesome_webfont_svg_len;
+extern "C" unsigned int __svg_font_ext__fonts__ExtJS_svg_len;
+extern "C" unsigned int __otf_font_awesome__fonts__FontAwesome_otf_len;
+extern "C" unsigned int __eot_font_ext__fonts__ExtJS_eot_len;
+extern "C" unsigned int __eot_font_awesome__fonts__fontawesome_webfont_eot_len;
+extern "C" unsigned int __css_classic__theme_triton__resources__theme_triton_all_debug_1_css_len;
+extern "C" unsigned int __css_classic__theme_triton__resources__theme_triton_all_debug_2_css_len;
+extern "C" unsigned int __css_ux__ux_all_debug_css_len;
+extern "C" unsigned int __woff_font_awesome__fonts__fontawesome_webfont_woff_len;
+extern "C" unsigned int __woff_font_ext__fonts__ExtJS_woff_len;
+extern "C" unsigned int __woff2_font_awesome__fonts__fontawesome_webfont_woff2_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_BoldItalic_ttf_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_SemiboldItalic_ttf_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_Italic_ttf_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_ExtraBoldItalic_ttf_len;
+extern "C" unsigned int __ttf_font_ext__fonts__ExtJS_ttf_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_ExtraBold_ttf_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_Regular_ttf_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_Semibold_ttf_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_Bold_ttf_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_LightItalic_ttf_len;
+extern "C" unsigned int __ttf_font_awesome__fonts__fontawesome_webfont_ttf_len;
+extern "C" unsigned int __ttf_fonts__OpenSans_Light_ttf_len;
+extern "C" unsigned int __js_classic__locale__locale_ukr_debug_js_len;
+extern "C" unsigned int __js_classic__locale__locale_ru_debug_js_len;
+extern "C" unsigned int __js_classic__locale__locale_en_debug_js_len;
+
+pair<string, pair<unsigned char*, unsigned int> > extfs_items[] = 
 {
     // Main modules
-    "/ext-all-debug.js",
-    "/ux-debug.js",
+    make_pair("/ext-all-debug.js", make_pair(__ext_all_debug_js, __ext_all_debug_js_len)),
+    make_pair("/ux-debug.js", make_pair(__ux_debug_js, __ux_debug_js_len)),
 
     // Stylesheet
-    "/ux/ux-all-debug.css",
-    "/classic/theme-triton/resources/theme-triton-all-debug_1.css",
-    "/classic/theme-triton/resources/theme-triton-all-debug_2.css",
+    make_pair("/ux/ux-all-debug.css", make_pair(__css_ux__ux_all_debug_css, __css_ux__ux_all_debug_css_len)),
+    make_pair("/classic/theme-triton/resources/theme-triton-all-debug_1.css", make_pair(__css_classic__theme_triton__resources__theme_triton_all_debug_1_css, __css_classic__theme_triton__resources__theme_triton_all_debug_1_css_len)),
+    make_pair("/classic/theme-triton/resources/theme-triton-all-debug_2.css", make_pair(__css_classic__theme_triton__resources__theme_triton_all_debug_2_css, __css_classic__theme_triton__resources__theme_triton_all_debug_2_css_len)),
 
     // Locales
-    "/classic/locale/locale-en-debug.js",
-    "/classic/locale/locale-ru-debug.js",
-    "/classic/locale/locale-ukr-debug.js",
+    make_pair("/classic/locale/locale-en-debug.js", make_pair(__js_classic__locale__locale_en_debug_js, __js_classic__locale__locale_ukr_debug_js_len)),
+    make_pair("/classic/locale/locale-ru-debug.js", make_pair(__js_classic__locale__locale_ru_debug_js, __js_classic__locale__locale_ukr_debug_js_len)),
+    make_pair("/classic/locale/locale-ukr-debug.js", make_pair(__js_classic__locale__locale_ukr_debug_js, __js_classic__locale__locale_ukr_debug_js_len)),
 
     // Fonts
-    "/fonts/OpenSans-BoldItalic.ttf",
-    "/fonts/OpenSans-Bold.ttf",
-    "/fonts/OpenSans-ExtraBoldItalic.ttf",
-    "/fonts/OpenSans-ExtraBold.ttf",
-    "/fonts/OpenSans-Italic.ttf",
-    "/fonts/OpenSans-LightItalic.ttf",
-    "/fonts/OpenSans-Light.ttf",
-    "/fonts/OpenSans-Regular.ttf",
-    "/fonts/OpenSans-SemiboldItalic.ttf",
-    "/fonts/OpenSans-Semibold.ttf",
-    "/font-awesome/fonts/FontAwesome.otf",
-    "/font-awesome/fonts/fontawesome-webfont.eot",
-    "/font-awesome/fonts/fontawesome-webfont.svg",
-    "/font-awesome/fonts/fontawesome-webfont.woff",
-    "/font-awesome/fonts/fontawesome-webfont.woff2",
-    "/font-awesome/fonts/fontawesome-webfont.ttf",
-    "/font-ext/fonts/ExtJS.ttf",
-    "/font-ext/fonts/ExtJS.woff",
-    "/font-ext/fonts/ExtJS.svg",
-    "/font-ext/fonts/ExtJS.eot",
+    make_pair("/fonts/OpenSans-BoldItalic.ttf", make_pair(__ttf_fonts__OpenSans_BoldItalic_ttf, __ttf_fonts__OpenSans_BoldItalic_ttf_len)),
+    make_pair("/fonts/OpenSans-Bold.ttf", make_pair(__ttf_fonts__OpenSans_Bold_ttf, __ttf_fonts__OpenSans_Bold_ttf_len)),
+    make_pair("/fonts/OpenSans-ExtraBoldItalic.ttf", make_pair(__ttf_fonts__OpenSans_ExtraBoldItalic_ttf, __ttf_fonts__OpenSans_ExtraBoldItalic_ttf_len)),
+    make_pair("/fonts/OpenSans-ExtraBold.ttf", make_pair(__ttf_fonts__OpenSans_ExtraBold_ttf, __ttf_fonts__OpenSans_ExtraBold_ttf_len)),
+    make_pair("/fonts/OpenSans-Italic.ttf", make_pair(__ttf_fonts__OpenSans_Italic_ttf, __ttf_fonts__OpenSans_Italic_ttf_len)),
+    make_pair("/fonts/OpenSans-LightItalic.ttf", make_pair(__ttf_fonts__OpenSans_LightItalic_ttf, __ttf_fonts__OpenSans_LightItalic_ttf_len)),
+    make_pair("/fonts/OpenSans-Light.ttf", make_pair(__ttf_fonts__OpenSans_Light_ttf, __ttf_fonts__OpenSans_Light_ttf_len)),
+    make_pair("/fonts/OpenSans-Regular.ttf", make_pair(__ttf_fonts__OpenSans_Regular_ttf, __ttf_fonts__OpenSans_Regular_ttf_len)),
+    make_pair("/fonts/OpenSans-SemiboldItalic.ttf", make_pair(__ttf_fonts__OpenSans_SemiboldItalic_ttf, __ttf_fonts__OpenSans_SemiboldItalic_ttf_len)),
+    make_pair("/fonts/OpenSans-Semibold.ttf", make_pair(__ttf_fonts__OpenSans_Semibold_ttf, __ttf_fonts__OpenSans_Semibold_ttf_len)),
+    make_pair("/font-awesome/fonts/FontAwesome.otf", make_pair(__otf_font_awesome__fonts__FontAwesome_otf, __otf_font_awesome__fonts__FontAwesome_otf_len)),
+    make_pair("/font-awesome/fonts/fontawesome-webfont.eot", make_pair(__eot_font_awesome__fonts__fontawesome_webfont_eot, __eot_font_awesome__fonts__fontawesome_webfont_eot_len)),
+    make_pair("/font-awesome/fonts/fontawesome-webfont.svg", make_pair(__svg_font_awesome__fonts__fontawesome_webfont_svg, __svg_font_awesome__fonts__fontawesome_webfont_svg_len)),
+    make_pair("/font-awesome/fonts/fontawesome-webfont.woff", make_pair(__woff_font_awesome__fonts__fontawesome_webfont_woff, __woff_font_awesome__fonts__fontawesome_webfont_woff_len)),
+    make_pair("/font-awesome/fonts/fontawesome-webfont.woff2", make_pair(__woff2_font_awesome__fonts__fontawesome_webfont_woff2, __woff2_font_awesome__fonts__fontawesome_webfont_woff2_len)),
+    make_pair("/font-awesome/fonts/fontawesome-webfont.ttf", make_pair(__ttf_font_awesome__fonts__fontawesome_webfont_ttf, __ttf_font_awesome__fonts__fontawesome_webfont_ttf_len)),
+    make_pair("/font-ext/fonts/ExtJS.ttf", make_pair(__ttf_font_ext__fonts__ExtJS_ttf, __ttf_font_ext__fonts__ExtJS_ttf_len)),
+    make_pair("/font-ext/fonts/ExtJS.woff", make_pair(__woff_font_ext__fonts__ExtJS_woff, __woff_font_ext__fonts__ExtJS_woff_len)),
+    make_pair("/font-ext/fonts/ExtJS.svg", make_pair(__svg_font_ext__fonts__ExtJS_svg, __svg_font_ext__fonts__ExtJS_svg_len)),
+    make_pair("/font-ext/fonts/ExtJS.eot", make_pair(__eot_font_ext__fonts__ExtJS_eot, __eot_font_ext__fonts__ExtJS_eot_len)),
 };
+
+unsigned int extfs_items_len = 27;
 
 bool get_file(const string& path, vector<unsigned char>& data, string& mimeType)
 {
-    
+    for(unsigned int i; i < extfs_items_len; i++)
+    {
+        if (path.size() < extfs_items[i].first.size())
+            continue;
+
+        if (strstr(path.c_str(), extfs_items[i].first.c_str()) != NULL)
+        {
+            data.assign(extfs_items[i].second.first, extfs_items[i].second.first + extfs_items[i].second.second);
+            return true;
+        }
+    }
 
     return false;
 }
