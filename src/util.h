05c208408e2ca3c72b3fd836e11dd1e6a549fbc3
CryptoManiac <balthazar@yandex.ru>
2016-04-11 01:27:05 +0300

    Revert commit 65c601e3aa895b0dd357513ef02314b86fc66173 for now.

--- src/util.h
+++ src/util.h
@@ -197,7 +197,8 @@ void ParseString(const std::string& str, char c, std::vector<std::string>& v);
 std::string FormatMoney(int64_t n, bool fPlus=false);
 bool ParseMoney(const std::string& str, int64_t& nRet);
 bool ParseMoney(const char* pszIn, int64_t& nRet);
-std::vector<uint8_t> ParseHex(const std::string& str);
+std::vector<uint8_t> ParseHex(const char *str);
+inline std::vector<uint8_t> ParseHex(const std::string& str) { return ParseHex(str.c_str()); }
 bool IsHex(const std::string& str);
 std::vector<unsigned char> DecodeBase64(const char* p, bool* pfInvalid = NULL);
 std::string DecodeBase64(const std::string& str);
