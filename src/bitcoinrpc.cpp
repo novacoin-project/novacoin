0a70d03e5a4e2e2dab28912ce7a961b71ffe3082
svost <ya.nowa@yandex.ru>
2016-04-14 10:36:25 +0300

    HexBits restyle

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -110,16 +110,15 @@ Value ValueFromAmount(int64_t amount)
     return (double)amount / (double)COIN;
 }
 
-std::string HexBits(unsigned int nBits)
+string HexBits(uint32_t nBits)
 {
     union {
-        int32_t nBits;
+        uint32_t nBits;
         char cBits[4];
     } uBits;
-    uBits.nBits = htonl((int32_t)nBits);
+    uBits.nBits = htonl(nBits);
     return HexStr(BEGIN(uBits.cBits), END(uBits.cBits));
 }
-
 
 //
 // Utilities: convert hex-encoded Values
