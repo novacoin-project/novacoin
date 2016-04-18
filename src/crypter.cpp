d561f2c30eb261867521e28fece7ec7956aa2bce
svost <ya.nowa@yandex.ru>
2016-04-18 10:23:06 +0300

    Just a memo: deal with -Wcast-qual check. int EVP_BytesToKey(const EVP_CIPHER *type,const EVP_MD *md,                    const unsigned char *salt,                    const unsigned char *data, int datal, int count,                    unsigned char *key,unsigned char *iv);

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -22,7 +22,7 @@ bool CCrypter::SetKeyFromPassphrase(const SecureString& strKeyData, const std::v
     if (nDerivationMethod == 0)
     {
         i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha512(), &chSalt[0],
-                          (unsigned char *)&strKeyData[0], strKeyData.size(), nRounds, chKey, chIV);
+                          (const unsigned char *)&strKeyData[0], strKeyData.size(), nRounds, chKey, chIV);
     }
 
     if (i != (int)WALLET_CRYPTO_KEY_SIZE)
