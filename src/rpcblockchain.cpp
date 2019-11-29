c36e0f10905a4de6613ea223ccfd6e17fbdc737b
Pavel Vasin <pavel@vasin.nl>
2019-11-29 15:10:43 +0200

    use cached block hash in blockToJSON()

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -102,7 +102,7 @@ double GetPoSKernelPS()
 Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool fPrintTransactionDetail)
 {
     Object result;
-    result.push_back(Pair("hash", block.GetHash().GetHex()));
+    result.push_back(Pair("hash", blockindex->GetBlockHash().GetHex()));
     CMerkleTx txGen(block.vtx[0]);
     txGen.SetMerkleBranch(&block);
     result.push_back(Pair("confirmations", (int)txGen.GetDepthInMainChain()));
