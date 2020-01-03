4d3613890446b786710afba0576834ce0a384630
Laser9un <laser9un@gmail.com>
2020-01-04 01:37:41 +0200

    Update Copyright Year

--- src/qt/locale/bitcoin_uk.ts
+++ src/qt/locale/bitcoin_uk.ts
@@ -16,16 +16,16 @@
     </message>
     <message>
         <location filename="../forms/aboutdialog.ui" line="97"/>
-        <source>Copyright © 2009-2016 The Bitcoin developers
+        <source>Copyright © 2009-2019 The Bitcoin developers
 Copyright © 2011-2012 The PPCoin Developers
 Copyright © 2014 The Peerunity Developers
 Copyright © 2014 The EmerCoin Developers
-Copyright © 2012-2019 The NovaCoin developers</source>
-        <translation>© 2009-2016 Розробники Bitcoin
+Copyright © 2012-2020 The NovaCoin developers</source>
+        <translation>© 2009-2019 Розробники Bitcoin
 © 2011-2012 Розробники PPCoin
 © 2014 Розробники Peerunity
 © 2014 Розробники EmerCoin
-© 2012-2019 Розробники NovaCoin</translation>
+© 2012-2020 Розробники NovaCoin</translation>
     </message>
     <message>
         <location filename="../forms/aboutdialog.ui" line="114"/>
