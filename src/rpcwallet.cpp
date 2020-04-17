f9ee348f60e5fd7ab79c30fcb2b7339c9bee1c31
CryptoManiac <CryptoManiac@users.noreply.github.com>
2020-04-18 01:05:46 +0300

    Fix getbalance() bug

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -581,7 +581,7 @@ int64_t GetAccountBalance(const string& strAccount, int nMinDepth, const isminef
 
 Value getbalance(const Array& params, bool fHelp)
 {
-    if (fHelp || params.size() > 2)
+    if (fHelp || params.size() > 3)
         throw runtime_error(
             "getbalance [account] [minconf=1] [watchonly=0]\n"
             "If [account] is not specified, returns the server's total available balance.\n"
