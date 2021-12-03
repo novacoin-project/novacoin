f7ad871507a82931ed5bc39a1196d3e839382b6c
svost <svost@users.noreply.github.com>
2021-12-03 11:34:26 +0300

    BN_is_prime -> BN_is_prime_ex

--- src/bignum.h
+++ src/bignum.h
@@ -605,7 +605,7 @@ public:
     */
     bool isPrime(const int checks=BN_prime_checks) const {
         CAutoBN_CTX pctx;
-        int ret = BN_is_prime(bn, checks, NULL, pctx, NULL);
+        int ret = BN_is_prime_ex(bn, checks, pctx, NULL);
         if(ret < 0){
             throw bignum_error("CBigNum::isPrime :BN_is_prime");
         }
