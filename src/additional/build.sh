803f99845f7d225777e9301fce857c8bbf72fa94
CryptoManiac <balthazar.ad@gmail.com>
2021-12-19 18:53:48 +0300

    Add leveldb submodule

--- src/additional/build.sh
+++ src/additional/build.sh
@@ -5,8 +5,15 @@ ROOT=$(pwd)
 git submodule update --init --recursive
 mkdir $ROOT/IXWebSocket/build
 cd $ROOT/IXWebSocket/build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/ ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/ -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 ..
 make -j 4
 make DESTDIR=$ROOT/stage install
 cd $ROOT
 
+mkdir $ROOT/leveldb/build
+cd $ROOT/leveldb/build
+git reset --hard 4cb80b7ddce6ff6089b15d8cfebf746fc1572477
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/ -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 ..
+make -j 4
+make DESTDIR=$ROOT/stage install
+cd $ROOT
